2485e1ea0dd33628d7387fda8a7a2ea794198583
ahmedalianz <ahmedalian400@gmail.com>
2021-11-19 14:16:14 +0200

    user Crud ops

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -18,5 +18,90 @@ class User{
             })
         }
     }
+    static showUser = async (req, res) => {
+        try{
+            const user = await userModel.findOne({_id:req.params.id})
+            res.status(200).send({
+                apiStatus:true,
+                data:user,
+                message:"user shown successfully"
+            })
+        }
+        catch(e) {
+            res.status(500).send({
+                apiStatus:false,
+                message:e.message
+            })
+        }
+    }
+    static showAllUsers = async (req, res) => {
+        try{
+            const allUsers = await userModel.find()
+            res.status(200).send({
+                apiStatus:true,
+                data:allUsers,
+                message:"data fetched successfully"
+            })
+        }
+        catch(e) {
+            res.status(500).send({
+                apiStatus:false,
+                message:e.message
+            })
+        }
+    }
+    static editUser = async (req, res) => {
+        try{
+            let user = await userModel.findByIdAndUpdate(req.params.id,{$set:req.body})
+            if(!user) throw new Error("user not found")
+            await user.save()
+            res.status(200).send({
+                apiStatus:true,
+                data:user,
+                message:"data edited successfully"
+            })
+        }
+        catch(e) {
+            res.status(500).send({
+                apiStatus:false,
+                message:e.message
+            })
+        }
+    }
+
+    static delUser = async (req, res) => {
+        try{
+            let user = await userModel.findByIdAndDelete(req.params.id)
+            if(!user) throw new Error("user not found")
+            res.status(200).send({
+                apiStatus:true,
+                message:"data deleted successfully"
+            })
+        }
+        catch(e) {
+            res.status(500).send({
+                apiStatus:false,
+                message:e.message
+            })
+        }
+    }
+
+    static delAll = async (req, res) => {
+        try{
+            await userModel.deleteMany()
+            res.status(200).send({
+                apiStatus:true,
+                message:"all data deleted successfully"
+            })
+        }
+        catch(e) {
+            res.status(500).send({
+                apiStatus:false,
+                message:e.message
+            })
+        }
+    }
+
+
 }
 module.exports=User
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const userController = require("../controller/user.controller")
 router.post('/register',userController.register); 
-
+router.get('/showUser',userController.showUser)
+router.get('/showAll',userController.showAllUsers)
+router.patch('/editUser',userController.editUser)
+router.delete('/deleteUser',userController.delUser)
+router.delete('/deleteAll',userController.delAll)
 module.exports=router
