edc86b7d9c9f4f13f3f490e151f8a9cab6a72168
ahmedalianz <ahmedalian400@gmail.com>
2021-11-19 17:00:18 +0200

    auth done

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const userController = require("../controller/user.controller")
+const auth = require("../middleware/auth")
 router.post('/register',userController.register);
-router.post('/login',userController.login); 
+router.post('/login',auth,userController.login); 
 router.get('/showUser/:id',userController.showUser)
 router.get('/showAll',userController.showAllUsers)
-router.patch('/editUser/:id',userController.editUser)
-router.delete('/deleteUser/:id',userController.delUser)
+router.patch('/editUser/:id',auth,userController.editUser)
+router.delete('/deleteUser/:id',auth,userController.delUser)
 router.delete('/deleteAll',userController.delAll)
 module.exports=router
