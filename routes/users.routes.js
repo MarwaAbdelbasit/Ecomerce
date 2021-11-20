72c9a3489e435440e322950800324a7f7ba686bb
ahmedalianz <ahmedalian400@gmail.com>
2021-11-20 13:17:27 +0200

    notes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,21 +2,19 @@ const router = require("express").Router();
 const userController = require("../controller/user.controller")
 const auth = require("../middleware/auth")
 const adminAuth = require("../middleware/admin.auth")
+//-----------------register/login for users -----------
 router.post('/register',userController.register);
 router.post('/login',userController.login); 
-router.get('/showUser/:id',userController.showUser)
-router.get('/showAll',adminAuth,userController.showAllUsers)
+//----------------user options to show/edit/delete/logout(1/many devices) his account---------
 router.get('/profile', auth, userController.profile)
 router.patch('/editUser/:id',auth,userController.editUser)
 router.delete('/deleteUser/:id',auth,userController.delUser)
-router.delete('/deleteAll',adminAuth,userController.delAll)
- 
-router.post('/placeOrder', auth, userController.placeOrder);
-router.patch('/editOrder/:orderId', auth, userController.editOrder);
-router.get('/allOrders', auth, userController.allOrders)
-router.get('/singleOrder/:orderId', auth, userController.singleOrder)
-router.delete('/delOrders', auth, userController.delOrders)
-router.delete('/delOrder/:orderId', auth, userController.delOrder)
+router.post('/logout', auth, userController.logout)
+router.post('/logoutall', auth, userController.logoutAll)
 
-module.exports=router
+//----------------admin options to show single/all registerd users/delete them-----
+router.get('/showUser/:id',adminAuth,userController.showUser)
+router.get('/showAll',adminAuth,userController.showAllUsers)
+router.delete('/deleteAll',adminAuth,userController.delAll)
+
 module.exports=router
