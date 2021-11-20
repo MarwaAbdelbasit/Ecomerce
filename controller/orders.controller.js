c7d3d089e7257b76e6dbaff6a4ccfbeeadff6b48
MarwaAbdelbasit <marwaabdelbasit3@gmail.com>
2021-11-20 16:15:47 +0200

    orders model crud operations

---
+++ controller/orders.controller.js
@@ -0,0 +1,90 @@
+const ordersModel = require("../db/models/orders.model")
+const successHandler = require("../helpers/successHandler")
+const errorHandler = require("../helpers/errorHandler")
+
+class Order {
+    static placeOrder = async (req, res) => {
+        try {
+            const order = await new ordersModel(req.body)
+            order.userId = req.user._id
+            order.save()
+            req.user.orders.push(order._id)
+            await req.user.save()
+            successHandler(req.user,res,'order placed successfully')
+        }
+        catch(e) {
+            errorHandler(e,res)
+        }
+    }
+
+    static editOrder = async (req, res) => {
+        try{
+            let allOrders = await ordersModel.find()
+            let orderIndex = allOrders.findIndex(o=>o._id==req.params.orderId)
+            if(orderIndex==-1) throw new Error("order not found")
+            allOrders[orderIndex]={
+                userId: req.user._id,
+                _id:req.params.orderId,
+                orderName:req.body.orderName,
+                amount:req.body.amount,
+                paid:req.body.paid,
+                delivered:req.body.delivered,
+            }
+            await allOrders.save()
+            successHandler(allOrders,res,'order edited successfully')
+        }
+        catch(e) {
+            errorHandler(e,res)
+        }
+    }
+
+    static allOrders = async (req, res) => {
+        try{
+            let allOrders = await ordersModel.find({userId:req.user._id})
+            if(allOrders.length==0) throw new Error("user have no orders")
+            successHandler(allOrders,res,'orders fetched successfully')
+        }
+        catch(e) {
+            errorHandler(e,res)
+        }
+    }
+
+    static singleOrder = async (req, res) => {
+        try{
+            let order = await ordersModel.findById(req.params.orderId)
+            if(!order) throw new Error("order not found")
+            successHandler(order,res,'order fetched successfully')
+        }
+        catch(e) {
+            errorHandler(e,res)
+        }
+    }
+
+    static delOrders = async (req, res) => {
+        try{
+            let allOrders = await ordersModel.find({userId:req.user._id})
+            if(allOrders.length==0) throw new Error("user have no orders")
+            allOrders = []
+            await allOrders.save()
+            successHandler(null,res,'orders deleted successfully')
+        }
+        catch(e) {
+            errorHandler(e,res)
+        }
+    }
+
+    static delOrder = async (req, res) => {
+        try{
+            let order = await ordersModel.findByIdAndDelete(req.params.orderId)
+            req.user.orders = req.user.orders.filter(o=>o._id!=req.params.orderId)
+            await req.user.save()
+            if(!order) throw new Error("no order to delete")
+            successHandler(order,res,'order deleted successfully')
+        }
+        catch(e) {
+            errorHandler(e,res)
+        }
+    }
+}
+
+module.exports = Order
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
-const userController = require("../controller/user.controller")
+const orderController = require("../controller/orders.controller")
 const auth = require("../middleware/auth")
 
 //----------------user options to control his order ----------------
-router.post('/placeOrder', auth, userController.placeOrder);
-router.patch('/editOrder/:orderId', auth, userController.editOrder);
-router.get('/allOrders', auth, userController.allOrders)
-router.get('/singleOrder/:orderId', auth, userController.singleOrder)
-router.delete('/delOrders', auth, userController.delOrders)
-router.delete('/delOrder/:orderId', auth, userController.delOrder)
+router.post('/placeOrder', auth, orderController.placeOrder);
+router.patch('/editOrder/:orderId', auth, orderController.editOrder);
+router.get('/allOrders', auth, orderController.allOrders)
+router.get('/singleOrder/:orderId', auth, orderController.singleOrder)
+router.delete('/delOrders', auth, orderController.delOrders)
+router.delete('/delOrder/:orderId', auth, orderController.delOrder)
 
 module.exports=router
