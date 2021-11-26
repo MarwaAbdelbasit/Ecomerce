ab3b461aa8de3127538aa5fb52e33989fd5245dc
ahmedalianz <ahmedalian400@gmail.com>
2021-11-26 15:30:37 +0200

    footer top added

---
+++ Client-Side/src/app/components/layout/footer/footer-middle/footer-middle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-middle',
+  templateUrl: './footer-middle.component.html',
+  styleUrls: ['./footer-middle.component.css']
+})
+export class FooterMiddleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
